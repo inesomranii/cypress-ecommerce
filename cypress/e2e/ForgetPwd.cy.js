ba85c8fee4fccc6974b19a1351b44f3829fc1b0e
inesomranii <omraniines060@gmail.com>
2024-03-29 13:12:16 +0100

    add file

---
+++ cypress/e2e/ForgetPwd.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+    it('forget', () => {
+     cy.visit('https://ecommerce-playground.lambdatest.io/')
+     cy.get('a[title="Poco Electro"]').should('be.visible')
+     cy.get('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=account/account"]').contains(' My account').click({force: true})
+     cy.get('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=account/forgotten"]').contains('Forgotten Password').click({force: true})
+     cy.get('input[id="input-email"]').type('ines@gmail.com')
+     cy.get('button[type="submit"]').contains('Continue').click({force: true})
+  
+})
+}) 
